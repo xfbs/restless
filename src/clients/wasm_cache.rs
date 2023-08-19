46836e2bd9ef8c6476b4f2c62a9fa56f08415fc0
Patrick Elsen <pelsen@xfbs.net>
2023-08-19 10:14:56 +0200

    Adds support for wasm-cache with invalidations

--- src/clients/wasm_cache.rs
+++ src/clients/wasm_cache.rs
@@ -1,7 +1,7 @@
 use crate::clients::gloo::GlooRequest;
 use async_trait::async_trait;
 use std::{borrow::Cow, fmt::Debug, hash::Hash};
-use wasm_cache::CacheItem;
+use wasm_cache::{Invalidatable, CacheItem};
 
 /// Cached request.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
@@ -12,11 +12,18 @@ pub struct CachedRequest<R: GlooRequest + Clone + Ord + Eq + Hash + Debug> {
     pub request: R,
 }
 
+impl<R: GlooRequest + Clone + Ord + Eq + Hash + Debug, M: 'static> Invalidatable<M> for CachedRequest<R> where R: Invalidatable<M> {
+    fn invalidated_by(&self, value: &M) -> bool {
+        self.request.invalidated_by(value)
+    }
+}
+
 #[async_trait(?Send)]
-impl<R: GlooRequest + PartialEq + Eq + PartialOrd + Ord + Hash + Clone + Debug + 'static>
-    CacheItem<()> for CachedRequest<R>
+impl<R: GlooRequest + PartialEq + Eq + PartialOrd + Ord + Hash + Clone + Debug + 'static, M: 'static>
+    CacheItem<M> for CachedRequest<R>
 where
     R::Response: Clone + Debug + PartialEq,
+    R: Invalidatable<M>,
 {
     type Value = R::Response;
     type Error = R::Error;
